8e23e98b6dd5517d8dd50d1f9c2e80a2769b5a53
tahasama <taha.maatof@gmail.com>
2022-09-04 23:12:12 +0100

    type writer animation

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import "./hero.css";
 
 const Hero = () => {
-  return <div className="line-1 anim-typewriter">Hero</div>;
+  return <div className="line-1 anim-typewriter">I'm a Hero</div>;
 };
 
 export default Hero;
