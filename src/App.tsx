3215a43143e165725dd7a8bb2f9c736d026547b1
tahasama <taha.maatof@gmail.com>
2022-09-07 19:53:48 +0100

    navbar in progress

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import "./App.css";
 import Hero from "./components/hero";
+import Navbar from "./components/navbar";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Navbar />
         <Hero />
       </header>
     </div>
