d8dd67c428885f7b65fe42a5f2a12a70de94c248
tahasama <taha.maatof@gmail.com>
2022-09-07 19:52:42 +0100

    add nav

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,11 @@
 import React from "react";
 import "./App.css";
 import Hero from "./components/hero";
-import Navbar from "./components/navbar";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        {/* <Navbar /> */}
         <Hero />
       </header>
     </div>
