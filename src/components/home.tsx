a18f3ea770614ed6a2611f662eb9e42a6c770f80
tahasama <taha.maatof@gmail.com>
2022-09-19 17:04:58 +0100

    redesign hero page with flex

--- src/components/home.tsx
+++ src/components/home.tsx
@@ -82,7 +82,7 @@ const Home = () => {
         flexDirection="column"
         position="relative"
         lineHeight={"taller"}
-        top={["-24", 0]}
+        top={["-36", 0]}
         w="auto"
         // alignItems={["center", "baseline"]}
       >
