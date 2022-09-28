3325f85237776ff9af1931e148ce7495386eb751
tahasama <taha.maatof@gmail.com>
2022-09-28 13:06:41 +0100

    skills page

--- src/App.tsx
+++ src/App.tsx
@@ -2,24 +2,15 @@ import { Box, useColorModeValue } from "@chakra-ui/react";
 import Navbar from "./components/navbar";
 import Home from "./components/home";
 import About from "./components/about";
-import Skills from "./components/skills";
-// import Home2 from "./components/home2";
 
 function App() {
   const colorOnLight = useColorModeValue("gray.800", "gray.200");
-  // const bglOnLights = useColorModeValue("blue.300", "gray.900");
 
   return (
-    <Box
-      // bg={bglOnLights}
-      // h="full"
-      color={colorOnLight}
-    >
+    <Box color={colorOnLight}>
       <Navbar />
       <Home />
-      {/* <Home2 /> */}
       <About />
-      {/* <Skills /> */}
     </Box>
   );
 }
