32553a277e407dd9d2982d2777d4967977a710b0
tahasama <taha.maatof@gmail.com>
2022-11-13 19:55:35 +0100

    fix vertical resize 2

--- src/components/about.tsx
+++ src/components/about.tsx
@@ -7,7 +7,7 @@ import {
   useColorMode,
   useColorModeValue,
 } from "@chakra-ui/react";
-import { m, motion } from "framer-motion";
+import { motion } from "framer-motion";
 import { useState } from "react";
 import { Element, Link } from "react-scroll";
 // import Projects from "./projects";
